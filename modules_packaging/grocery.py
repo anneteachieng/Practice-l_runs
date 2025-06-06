f4e9f19eea712058902926e173658d9afd208df5
anneteachieng <achiengomondi02@gmail.com>
2025-06-06 23:01:22 +0300

    modules and programs

---
+++ modules_packaging/grocery.py
@@ -0,0 +1,6 @@
+#grocery shopping
+
+import shopping_list
+
+result = shopping_list.grocery(2, 1, 7, 8, 2)
+print(result)
---
+++ modules_packaging/main.py
@@ -0,0 +1,9 @@
+#program
+import math_functions
+
+result = math_functions.add(10, 5)
+print(f"The sum is: {result}")
+
+
+result = math_functions.multiply(10, 5)
+print(f"The product is: {result}")
