46b1708835616ea52c075338c93c600f4a613dc1
Annete <achiengomondi02@gmail.com>
2025-06-28 00:57:34 +0300

    mutable sets

---
+++ data_strucures/fruit_list.py
@@ -0,0 +1,6 @@
+#Create a list to store names of your favorite fruits. Write code to access the second element and print it.
+
+favorite_fruits = ['lime', 'banana', 'passion', 'grapes', 'melons', 'guava']
+
+print(favorite_fruits[1])
+
