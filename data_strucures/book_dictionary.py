0eb8af519f030806b4a58fdd2a5fdd92d2375f73
Annete <achiengomondi02@gmail.com>
2025-06-28 02:06:30 +0300

    dictionary

---
+++ data_strucures/book_dictionary.py
@@ -0,0 +1,4 @@
+#Define a dictionary to store information about your favorite book, including title, author, and genre. Use the method to retrieve the genre
+
+favorite_book = {'title':'Archangels Consort', 'author':'Nalini Singh', 'genre':'Paranormal Romance', 'publication':'Berkley Books', 'series':'Guild Hunter series'}
+print(favorite_book.get('genre'))
